ae7e6553a162248bfc3ba50c85ae41fdb831decd
Darzi-Rin <73325373+Darzi-Rin@users.noreply.github.com>
2021-09-17 10:57:57 +0900

    django基盤完成

--- config/settings.py
+++ config/settings.py
@@ -37,11 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
-    'satou.apps.SatouConfig',
-    'kikuchi.apps.KikuchiConfig',
-    'hinata.apps.HinataConfig',
 ]
 
 MIDDLEWARE = [
