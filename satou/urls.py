a6ec67d5532cdf0489352f41e8398aafaebbd7bf
Darzi-Rin <73325373+Darzi-Rin@users.noreply.github.com>
2021-09-17 11:00:32 +0900

    delete_satou

--- satou/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from . import views
-app_name='diary'
-URLPatterns=[
-    path('',views.IndexView.as_view(),name='index'),
-]
